12a1d5edb1db71bb528c3a13873d6cbe0a9cf1ac
Andres Gardiol <andresgardiol@gmail.com>
2021-07-28 21:51:03 -0300

    Add meta tags

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import './App.css';
-import {useLayoutEffect, useState} from "react";
+import {useEffect, useLayoutEffect, useState} from "react";
 import QRCode from "react-qr-code";
 import Editor from "react-simple-code-editor";
 import {highlight, languages} from "prismjs/components/prism-core";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
 import "prismjs/themes/prism.css";
+import {createBrowserHistory} from 'history';
+
+export const history = createBrowserHistory();
 
 function App() {
 
-    let [text, setText] = useState(`Enter your text here...`);
+    let [text, setText] = useState(``);
     let [width] = useWindowSize();
+    let queryParams = useQueryParams();
+
+    useEffect(() => {
+        let defaultText = 'Enter your text here...';
+        let queryParam = queryParams['text'];
+        if (queryParam) defaultText = decodeURIComponent(queryParam);
+        setText(defaultText);
+    }, []);
+
+    useEffect(() => {
+        setQueryParam('text', text);
+    }, [text]);
+
 
     function handleChangeInput(text) {
         setText(text);
@@ -34,11 +50,63 @@ function App() {
 }
 
 function getSize(width) {
-    if(!width) return 100;
-    if(width > 500) return 500;
+    if (!width) return 100;
+    if (width > 500) return 500;
     return width - 100;
 
 }
+
+export const useQueryParams = () => {
+    const [queryParams, setQueryParams] = useState(getQueryParamsObject(history.location.search));
+
+    useEffect(() => {
+        function listener({location}) {
+            setQueryParams(getQueryParamsObject(location.search));
+        }
+
+        let unlisten = history.listen(listener);
+
+        return () => unlisten();
+    });
+    return queryParams;
+}
+
+export const getQueryParamsObject = (searchString) => {
+    let pairs = searchString.substring(1).split("&");
+    let obj = {};
+    let pair;
+
+    function getValue(valueString) {
+        if (valueString.includes('[')) {
+            valueString = valueString.replace('[', '');
+            valueString = valueString.replace(']', '');
+            return valueString.split(',').map(value => {
+                function isNumeric(num) {
+                    return !isNaN(num)
+                }
+
+                if (isNumeric(value)) return +value;
+                return value;
+            });
+        }
+        return pair[1];
+    }
+
+    for (let i in pairs) {
+        if (pairs[i] === "") continue;
+
+        pair = pairs[i].split("=");
+        let value = getValue(decodeURIComponent(pair[1]));
+        obj[decodeURIComponent(pair[0])] = value;
+    }
+
+    return obj;
+}
+
+export const setQueryParam = (key, value) => {
+    history.push({search: `?${encodeURIComponent(key)}=${encodeURIComponent(value)}`, pathname: ''})
+}
+
 
 function useWindowSize() {
     let [size, setSize] = useState([0, 0]);
